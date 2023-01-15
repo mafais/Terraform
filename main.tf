e842d3f52ea73c3d5fbf1ece46cad4d35aa8e2b3
mafais <mafais@outlook.com>
2023-01-15 14:50:43 +0530

    terraform resource files

---
+++ main.tf
@@ -0,0 +1,7 @@
+ 
+
+resource "azurerm_resource_group" "rg" {
+  location = var.resource_group_location
+  name     = var.resource_group_name
+
+}
---
+++ variables.tf
@@ -0,0 +1,16 @@
+variable "resource_group_location" {
+  default     = "eastus"
+  description = "Location of the resource group."
+}
+
+variable "resource_group_name_prefix" {
+  default     = "rg"
+  description = "Prefix of the resource group name that's combined with a random ID so name is unique in your Azure subscription."
+}
+
+variable "resource_group_name" {
+  default     = "AVD"
+  description = "The resource group name that's combined with a random ID so name is unique in your Azure subscription."
+}
+
+ 
